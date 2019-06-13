4e7f008b156fc98bcc5b006a02ccfd0ef3142813
Andrew Rimpici <andrew.rimpici@mymail.champlain.edu>
2019-06-12 22:43:12 -0400

    Started character stuff, but wanna push karims changes before I continue.

---
+++ Dev/Sigiriya/Assets/Code/Framework/Game/CharacterTracker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTracker : MonoBehaviour
+{
+
+}
+
+public enum EnumCharacterEmotion : int
+{
+
+}
