2c9cfc691ccd0362c7c271b27bb7889f874c78b1
Andrew Rimpici <andrew.rimpici@mymail.champlain.edu>
2019-07-10 01:29:39 -0400

    Added in map

--- Dev/Sigiriya/Assets/Code/Framework/Game/Map/MapNavigation.cs
+++ Dev/Sigiriya/Assets/Code/Framework/Game/Map/MapNavigation.cs
@@ -4,16 +4,58 @@ using UnityEngine;
 
 public class MapNavigation : MonoBehaviour
 {
+    [SerializeField] private GameObject map;
+
+    private void OnEnable()
+    {
+        EventAnnouncer.OnEndFadeIn += CloseMap;
+    }
+
+    private void OnDisable()
+    {
+        EventAnnouncer.OnEndFadeIn -= CloseMap;
+    }
+
+    public void GoToKitchen(bool shouldFade)
+    {
+        EventAnnouncer.OnRequestLocationChange?.Invoke(EnumLocation.KITCHEN, shouldFade);
+    }
+
+    public void GoToConstructionSite(bool shouldFade)
+    {
+        EventAnnouncer.OnRequestLocationChange?.Invoke(EnumLocation.CONSTRUCTION, shouldFade);
+    }
+
+    public void GoToGarden(bool shouldFade)
+    {
+        EventAnnouncer.OnRequestLocationChange?.Invoke(EnumLocation.GARDEN, shouldFade);
+    }
+
+    public void GoToGatheringSpace(bool shouldFade)
+    {
+        EventAnnouncer.OnRequestLocationChange?.Invoke(EnumLocation.GATHERING, shouldFade);
+    }
+
     public void GoToHome(bool shouldFade)
     {
-        Debug.Log ("GO TO HOME");
-        EventAnnouncer.OnRequestLocationChange?.Invoke (EnumLocation.HOME, shouldFade);
+        EventAnnouncer.OnRequestLocationChange?.Invoke(EnumLocation.HOME, shouldFade);
     }
 
     public void GoToSpring(bool shouldFade)
     {
-        Debug.Log ("GO TO SPRING");
-        EventAnnouncer.OnRequestLocationChange?.Invoke (EnumLocation.SPRING, shouldFade);
+        EventAnnouncer.OnRequestLocationChange?.Invoke(EnumLocation.SPRING, shouldFade);
     }
 
+    public void ToggleMap()
+    {
+        map.SetActive(!map.activeSelf);
+    }
+
+    private void CloseMap(string fadeID)
+    {
+        if (fadeID.CompareTo("location_fade") == 0)
+        {
+            map.SetActive(false);
+        }
+    }
 }
