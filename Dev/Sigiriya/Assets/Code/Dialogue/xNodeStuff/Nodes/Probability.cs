02dcb4ea5be0e8d8cb32e4d1726e12e92075c4f4
Lunick <karimelzanaty98@gmail.com>
2019-08-04 19:36:08 -0400

    Added quick prob node

---
+++ Dev/Sigiriya/Assets/Code/Dialogue/xNodeStuff/Nodes/Probability.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[NodeTint(46, 217, 125)]
+[CreateNodeMenu("Sigiriya/Logic/Probability")]
+public class Probability : Node {
+
+	[Input(typeConstraint = TypeConstraint.Strict)] public bool arg1;
+	[Output] public bool result;
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port)
+	{
+		bool arg1 = GetInputValue<bool>("arg1", this.arg1);
+
+		result = arg1;
+
+		return result;
+	}
+}
