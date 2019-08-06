382c13f0a0812451f405ed9240090f4f4845ab32
Lunick <karimelzanaty98@gmail.com>
2019-08-06 02:14:47 -0400

    Probab node works. Still need time for no return

--- Dev/Sigiriya/Assets/Code/Dialogue/xNodeStuff/Nodes/Probability.cs
+++ Dev/Sigiriya/Assets/Code/Dialogue/xNodeStuff/Nodes/Probability.cs
@@ -10,12 +10,32 @@ public class Probability : Node {
 	[Input(typeConstraint = TypeConstraint.Strict)] public bool arg1;
 	[Output] public bool result;
 
+	public int probability = 50;
+	public int modVal;
+
 	// Return the correct value of an output port when requested
 	public override object GetValue(NodePort port)
 	{
 		bool arg1 = GetInputValue<bool>("arg1", this.arg1);
 
-		result = arg1;
+		int calcProb = 0;
+
+		if (arg1)
+		{
+			calcProb = probability + modVal;
+		}
+		else
+		{
+			calcProb = probability;
+		}
+
+		int num = Random.Range(0, 100);
+
+		if (num < calcProb)
+		{
+			result = true;
+		}
+		else result = false;
 
 		return result;
 	}
