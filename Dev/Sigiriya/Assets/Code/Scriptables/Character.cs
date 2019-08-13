4b66607e8f059fd6f9580cffe706f8234dd12cb1
Tom Lunday <thomas.lunday@mymail.champlain.edu>
2019-08-13 13:23:40 -0400

    Character changes

--- Dev/Sigiriya/Assets/Code/Scriptables/Character.cs
+++ Dev/Sigiriya/Assets/Code/Scriptables/Character.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Can be used to create new Characters by designers or artists.
-[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+[CreateAssetMenu (fileName = "New Character", menuName = "Character")]
 public class Character : ScriptableObject
 {
     //  - traits
@@ -14,25 +14,25 @@ public class Character : ScriptableObject
     //Status effects take precedence over current emotional state
 
     //This is the stuff implemented by the designers/artists
-        
+
     public string characterName;
     public Sprite defaultSprite;
 
-    public List<EnumExpression> rawExpressionOrder = new List<EnumExpression>();
-    public List<Sprite> rawExpressions = new List<Sprite>();
-    public List<Sprite> rawTalkExpressions = new List<Sprite>();
+    public List<EnumExpression> rawExpressionOrder = new List<EnumExpression> ();
+    public List<Sprite> rawExpressions = new List<Sprite> ();
+    public List<Sprite> rawTalkExpressions = new List<Sprite> ();
 
-    public List<SubTrait> rawSubTraits = new List<SubTrait>();
+    public List<SubTrait> rawSubTraits = new List<SubTrait> ();
 
 
     /////////////////////////////////////////////
     // This is the stuff we access in code
 
-    private HashSet<SubTrait> subTraits = new HashSet<SubTrait>();
+    private HashSet<SubTrait> subTraits = new HashSet<SubTrait> ();
     private HashSet<SuperTrait> superTraits;
 
-    private Dictionary<EnumExpression, Sprite> expressions = new Dictionary<EnumExpression, Sprite>();
-    private Dictionary<EnumExpression, Sprite> talkExpressions = new Dictionary<EnumExpression, Sprite>();
+    private Dictionary<EnumExpression, Sprite> expressions = new Dictionary<EnumExpression, Sprite> ();
+    private Dictionary<EnumExpression, Sprite> talkExpressions = new Dictionary<EnumExpression, Sprite> ();
 
     //MoodTracker.AddMood
     //MoodTracker.RemoveMood
@@ -42,36 +42,36 @@ public class Character : ScriptableObject
     //MoodNode.Paused
     //MoodeNod.Duration
     //MoodNode.Mood
-    public MoodTracker MoodTracker = new MoodTracker();
+    public MoodTracker MoodTracker = new MoodTracker ();
 
     public void InitCharacter()
     {
-        RegisterSubTraits();
-        RegisterSuperTraits();
-        MoodController.Instance.RegisterMoodTracker(this, MoodTracker);
-        InitSprites();
+        RegisterSubTraits ();
+        RegisterSuperTraits ();
+        MoodController.Instance.RegisterMoodTracker (this, MoodTracker);
+        InitSprites ();
     }
 
     private void RegisterSubTraits()
     {
-        subTraits.Clear();
+        subTraits.Clear ();
         foreach (SubTrait trait in rawSubTraits)
         {
-            if (!subTraits.Add(trait))
+            if (!subTraits.Add (trait))
             {
-                Debug.Log("Sub Trait: " + trait.traitName + " is already registered.");
+                Debug.Log ("Sub Trait: " + trait.traitName + " is already registered.");
             }
         }
     }
 
     private void RegisterSuperTraits()
     {
-        superTraits = TraitController.Instance.GetSuperTraits(in subTraits);
+        superTraits = TraitController.Instance.GetSuperTraits (in subTraits);
 
-        Debug.Log("Super Traits for: " + characterName);
+        Debug.Log ("Super Traits for: " + characterName);
         foreach (SuperTrait superTrait in superTraits)
         {
-            Debug.Log("Trait: " + superTrait);
+            Debug.Log ("Trait: " + superTrait);
         }
     }
 
@@ -81,29 +81,30 @@ public class Character : ScriptableObject
         bool fail = false;
         if (rawExpressionOrder.Count != rawExpressions.Count)
         {
-            Debug.Log("Make sure there are the correct amount of expression sprites for the ordering.");
+            Debug.Log ("Make sure there are the correct amount of expression sprites for the ordering.");
             fail = true;
         }
 
         if (rawExpressionOrder.Count != rawTalkExpressions.Count)
         {
-            Debug.Log("Make sure there are the correct amount of TALK expression sprites for the ordering.");
+            Debug.Log ("Make sure there are the correct amount of TALK expression sprites for the ordering.");
             fail = true;
         }
 
-        expressions.Clear();
-        talkExpressions.Clear();
+        expressions.Clear ();
+        talkExpressions.Clear ();
 
-        if (fail) return;
+        if (fail)
+            return;
 
-        for (int i = 0; i < rawExpressions.Count; ++i)
+        for (int i = 0 ; i < rawExpressions.Count ; ++i)
         {
-            expressions.Add(rawExpressionOrder[i], rawExpressions[i]);
+            expressions.Add (rawExpressionOrder [i], rawExpressions [i]);
         }
 
-        for (int i = 0; i < rawTalkExpressions.Count; ++i)
+        for (int i = 0 ; i < rawTalkExpressions.Count ; ++i)
         {
-            talkExpressions.Add(rawExpressionOrder[i], rawTalkExpressions[i]);
+            talkExpressions.Add (rawExpressionOrder [i], rawTalkExpressions [i]);
         }
     }
 
@@ -130,7 +131,7 @@ public class Character : ScriptableObject
             }
         }
 
-        Debug.Log("COULDN'T FIND SPRITE FOR EXPRESSION: " + expression + ". Returning default sprite.");
+        Debug.Log ("COULDN'T FIND SPRITE FOR EXPRESSION: " + expression + ". Returning default sprite.");
         return defaultSprite;
     }
 
@@ -143,32 +144,29 @@ public class Character : ScriptableObject
     public enum EnumExpression
     {
         ANGRY,
-        SQUINTY_SMILE,
-        SURPRISED,
-        TIRED,
-        HAPPY,
-        NEUTRAL,
-        SMUG,
-        THINK,
-        OH_REALLY,
-        SMILE,
-        SATISFIED,
-        UNIMPRESSED,
-        FIST,
-        LOOK_AWAY,
-        SCARED,
-        EXPLAIN,
-        SECRETS,
-        CLOSED_OFF,
-        OPENING_UP,
-        CRY,
-        AMUSED,
-        ARMS_OPEN,
-        GRUMPY,
+        ANNOYED,
+        BASHFUL,
+        BIG_SMILE,
+        BOTHERED,
+        CURIOUS,
+        EMBARRASED,
+        EXITED,
+        EYEBROWS,
+        EYEROLL,
+        FLUSTERED,
+        HURT,
         LAUGH,
-        STERN,
-        FRUSTRATED,
-        INTERESTED,
-        SAD
+        NERVOUS,
+        NEUTRAL,
+        SAD,
+        SERIOUS,
+        SMALL_SMILE,
+        SMIRK,
+        SMUG,
+        STARTLED,
+        SURPRISED,
+        THOUGHTFUL,
+        UNSURE,
+        SIZE
     }
 }
