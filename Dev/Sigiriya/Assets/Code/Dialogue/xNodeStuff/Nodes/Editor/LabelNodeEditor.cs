5cd90ebdd9b19b958e206be1dabf09b771416eac
Lunick <karimelzanaty98@gmail.com>
2019-08-13 21:36:09 -0400

    Added label nodes

---
+++ Dev/Sigiriya/Assets/Code/Dialogue/xNodeStuff/Nodes/Editor/LabelNodeEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNodeEditor;
+using UnityEditor;
+
+[CustomNodeEditor(typeof(Label))]
+public class LabelNodeEditor : NodeEditor
+{
+	public override void OnBodyGUI()
+	{
+		serializedObject.Update();
+
+		Label node = target as Label;
+
+		NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("label"), GUIContent.none);
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+}
---
+++ Dev/Sigiriya/Assets/Code/Dialogue/xNodeStuff/Nodes/LabelNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[NodeTint(255,0,183)]
+[CreateNodeMenu("Sigiriya/Label")]
+public class Label : Node
+{
+	public string label;
+
+	// Use this for initialization
+	protected override void Init()
+	{
+		base.Init();
+		
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port)
+	{
+		return null; // Replace this
+	}
+}
