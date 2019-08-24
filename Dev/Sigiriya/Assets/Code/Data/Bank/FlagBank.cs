9a09eb43f7a115381a380f852006346dbe9223a8
Lunick <karimelzanaty98@gmail.com>
2019-08-23 22:36:32 -0400

    Updated flagbank

--- Dev/Sigiriya/Assets/Code/Data/Bank/FlagBank.cs
+++ Dev/Sigiriya/Assets/Code/Data/Bank/FlagBank.cs
@@ -8,54 +8,27 @@ public class FlagBank : ManagerBase<FlagBank>
 	public enum Flags
 	{
 		NONE,
-		_Chamara,
-		cham_Interest,
-		cham_Routine,
-		cham_Squabble,
-		cham_Tease,
-		_Gayesha,
-		gaye_SoftInsult,
-		gaye_Incapable,
-		gaye_LakminiTime,
-		_Lakmini,
-		lak_Better,
-		lak_Smile,
-		lak_Same,
-		lak_Worse,
-		lak_contribution,
-		_Isuri,
-		isu_Busy,
-		isu_SaidLeave,
-		isu_Bother,
-		isu_Childish,
-		isu_Left,
-		isu_Angry,
-		_Neja,
-		neja_map,
-		neja_momDead,
-		neja_scarab,
-		_Kalathma,
-		kal_talkWife,
-		Kal_weirdAdvice,
-		kal_worry,
-		_BigBitch,
-		Its,
-		Me,
-		Im,
-		The,
-		Big,
-		Bitch
+		_Toms_Flags,
+		Trust,
+		Selling_Something,
+		Chamara_Works,
+		Asked_Gayesha_To_Cook,
+		Lakmini_Is_Welcome,
+		Learned_Truth,
+		Told_Truth,
+		Comforted_Gayesha,
+		Saw_Pamu_Drunk,
+		_Test_Flags,
+		Alpha,
+		Bravo,
+		Charlie,
+		Dick
 	}
 
 	[HideInInspector]
 	public static List<Flags> submenuList = new List<Flags>
 	{
-		Flags._Chamara,
-		Flags._Gayesha,
-		Flags._Lakmini,
-		Flags._Isuri,
-		Flags._Neja,
-		Flags._Kalathma,
-		Flags._BigBitch
+		FlagBank.Flags._Toms_Flags,
+		FlagBank.Flags._Test_Flags
 	};
 }
